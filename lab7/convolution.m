ad067f5e0e347d4187dead1f77468da6f56b743e
HubertFraczek <hhubcio12@gmail.com>
2019-11-12 18:29:50 +0100

    zad1 lab7

--- lab7/convolution.m
+++ lab7/convolution.m
@@ -1,5 +1,5 @@
 function data_filtered = convolution(data, local_window)
-    FUNCTION = @(data_, local_window)convolution_local(data_, local_window_);
+    FUNCTION = @(data_, local_window_)convolution_local(data_, local_window_);
     
     data_filtered = colfilt(data, local_window, 'sliding', FUNCTION, local_window);
 end
--- lab7/zad1.m
+++ lab7/zad1.m
@@ -3,3 +3,15 @@ clear all; clearvars; clc;
 load MR_data.mat;
 
 local_window = [4 4];
+
+figure;
+subplot(3,2,1)
+imshow(uint8(convolution(I_noisefree, local_window)), [])
+subplot(3,2,2)
+imshow(uint8(convolution(I_noisy1, local_window)), [])
+subplot(3,2,3)
+imshow(uint8(convolution(I_noisy2, local_window)), [])
+subplot(3,2,4)
+imshow(uint8(convolution(I_noisy3, local_window)), [])
+subplot(3,2,5)
+imshow(uint8(convolution(I_noisy4, local_window)), [])
